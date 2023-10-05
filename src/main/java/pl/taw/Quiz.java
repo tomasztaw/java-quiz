ad5902db953296285c0a111abb1bc336ef1f27a2
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-10-05 18:26:31 +0200

    The constructor, nextQuestion and actionEvent are ready.

--- src/main/java/pl/taw/Quiz.java
+++ src/main/java/pl/taw/Quiz.java
@@ -43,32 +43,180 @@ public class Quiz implements ActionListener {
     JLabel seconds_left = new JLabel();
     JTextField number_right = new JTextField();
     JTextField percentage = new JTextField();
+    Font ubuntuMonoTextArea = new Font("Ubuntu Mono", Font.BOLD, 25);
+    Font ubuntuMonoButton = new Font("Ubuntu Mono", Font.BOLD, 35);
+    Color darkColor = new Color(50, 50, 50);
+    Color greenLight = new Color(25, 255, 0);
+    Color blackBlack = new Color(25, 25, 25);
+
 
     public Quiz() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(650, 650);
-        frame.getContentPane().setBackground(new Color(50, 50, 50));
+        frame.getContentPane().setBackground(darkColor);
         frame.setLayout(null);
         frame.setResizable(false);
 
         textField.setBounds(0, 0, 650, 50);
-        textField.setBackground(new Color(25, 25, 25));
-        textField.setForeground(new Color(25, 255, 0));
+        textField.setBackground(blackBlack);
         textField.setFont(new Font("Roboto", Font.BOLD, 30));
         textField.setBorder(BorderFactory.createBevelBorder(1));
         textField.setHorizontalAlignment(JTextField.CENTER);
         textField.setEditable(false);
 
+        textArea.setBounds(0, 50, 650, 50);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBackground(blackBlack);
+        textArea.setForeground(greenLight);
+        textArea.setFont(ubuntuMonoTextArea);
+        textArea.setBorder(BorderFactory.createBevelBorder(1));
+        textArea.setEditable(false);
+
+        buttonA.setBounds(0, 100, 100, 100);
+        buttonA.setFont(ubuntuMonoButton);
+        buttonA.setFocusable(false);
+        buttonA.addActionListener(this);
+        buttonA.setText("A");
+
+        buttonB.setBounds(0, 200, 100, 100);
+        buttonB.setFont(ubuntuMonoButton);
+        buttonB.setFocusable(false);
+        buttonB.addActionListener(this);
+        buttonB.setText("B");
+
+        buttonC.setBounds(0, 300, 100, 100);
+        buttonC.setFont(ubuntuMonoButton);
+        buttonC.setFocusable(false);
+        buttonC.addActionListener(this);
+        buttonC.setText("C");
+
+        buttonD.setBounds(0, 400, 100, 100);
+        buttonD.setFont(ubuntuMonoButton);
+        buttonD.setFocusable(false);
+        buttonD.addActionListener(this);
+        buttonD.setText("D");
+
+        answer_labelA.setBounds(125, 100, 500, 100);
+        answer_labelA.setBackground(darkColor);
+        answer_labelA.setForeground(greenLight);
+        answer_labelA.setFont(ubuntuMonoButton);
+
+        answer_labelB.setBounds(125, 200, 500, 100);
+        answer_labelB.setBackground(darkColor);
+        answer_labelB.setForeground(greenLight);
+        answer_labelB.setFont(ubuntuMonoButton);
+
+        answer_labelC.setBounds(125, 300, 500, 100);
+        answer_labelC.setBackground(darkColor);
+        answer_labelC.setForeground(greenLight);
+        answer_labelC.setFont(ubuntuMonoButton);
+
+        answer_labelD.setBounds(125, 400, 500, 100);
+        answer_labelD.setBackground(darkColor);
+        answer_labelD.setForeground(greenLight);
+        answer_labelD.setFont(ubuntuMonoButton);
+
+        seconds_left.setBounds(535, 510, 100, 100);
+        seconds_left.setBackground(blackBlack);
+        seconds_left.setForeground(Color.red);
+        seconds_left.setFont(new Font("Roboto", Font.ITALIC, 60));
+        seconds_left.setBorder(BorderFactory.createBevelBorder(1));
+        seconds_left.setOpaque(true);
+        seconds_left.setHorizontalAlignment(JTextField.CENTER);
+        seconds_left.setText(String.valueOf(seconds));
+
+        time_label.setBounds(535, 475, 100, 25);
+        time_label.setBackground(darkColor);
+        time_label.setForeground(Color.red);
+        time_label.setFont(new Font("Roboto", Font.PLAIN, 16));
+        time_label.setHorizontalAlignment(JTextField.CENTER);
+        time_label.setText("timer >:D");
+
+        number_right.setBounds(225, 225, 200, 100);
+        number_right.setBackground(blackBlack);
+        number_right.setForeground(Color.green);
+        number_right.setFont(new Font("Ubuntu Mono", Font.BOLD, 50));
+        number_right.setBorder(BorderFactory.createBevelBorder(1));
+        number_right.setHorizontalAlignment(JTextField.CENTER);
+        number_right.setEditable(false);
+
+        percentage.setBounds(225, 325, 200, 100);
+        percentage.setBackground(blackBlack);
+        percentage.setForeground(Color.green);
+        percentage.setFont(new Font("Ubuntu Mono", Font.BOLD, 50));
+        percentage.setBorder(BorderFactory.createBevelBorder(1));
+        percentage.setHorizontalAlignment(JTextField.CENTER);
+        percentage.setEditable(false);
+
+        frame.add(time_label);
+        frame.add(seconds_left);
+        frame.add(answer_labelA);
+        frame.add(answer_labelB);
+        frame.add(answer_labelC);
+        frame.add(answer_labelD);
+        frame.add(buttonA);
+        frame.add(buttonB);
+        frame.add(buttonC);
+        frame.add(buttonD);
+        frame.add(textArea);
         frame.add(textField);
         frame.setVisible(true);
+
+        nextQuestion();
     }
 
     public void nextQuestion() {
 
+        if (index >= total_question) {
+            result();
+        } else {
+            textField.setText("Question " + (index + 1));
+            textArea.setText(questions[index]);
+            answer_labelA.setText(options[index][0]);
+            answer_labelB.setText(options[index][1]);
+            answer_labelC.setText(options[index][2]);
+            answer_labelD.setText(options[index][3]);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        if (e.getSource() == buttonA) {
+            answer = 'A';
+            if (answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+
+        if (e.getSource() == buttonB) {
+            answer = 'B';
+            if (answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+
+        if (e.getSource() == buttonC) {
+            answer = 'C';
+            if (answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+
+        if (e.getSource() == buttonD) {
+            answer = 'D';
+            if (answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+
+        displayAnswer();
 
     }
 
